6f8e2191d9875c724e4302b6c057c72465b2d70b
Bappi Mazumder <bappi.itsd@goldenharvestbd.com>
2025-12-12 19:26:56 +0600

    main: implment list rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { useState,useEffect } from 'react'
 import './App.css'
-import Clock from './components/Clock'
+// import Clock from './components/Clock'
+import ClockList from './components/ClockList';
 
 function App() {
   console.log("App component rendered");
 
-  return <Clock />;
+  // return <Clock />;
+  const quantities = [1,2,3];
+  return (
+      <div>
+        <ClockList quantities={quantities} />
+      </div>
+  );
 }
 
-export default Clock;
+export default App;
---
+++ src/components/ClockList.jsx
@@ -0,0 +1,23 @@
+import Clock from "./Clock";
+
+export default function ClockList({quantities = [] }){
+
+    return(
+        <>
+            <div>
+            {quantities.map((key,index) => (
+                <Clock key={Math.random()} />
+            ))}
+            </div>
+            <div>
+            {quantities.map((key,index) => (
+                <Clock key={key} />
+            ))}
+            </div>
+       </>
+       
+    );
+
+}
+
+Next 10 
