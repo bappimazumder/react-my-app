d150c518027cc849c1f01777958b8ad950aea3c4
Bappi Mazumder <bappi.itsd@goldenharvestbd.com>
2025-12-22 13:35:16 +0600

    main: implement textarea and select type form handing

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,23 +3,53 @@ import React from 'react';
 
 export default class Form extends React.Component {
     state = {
-        title: 'Javascript'
+        title: 'Javascript',
+        text: 'JavaScript is great',
+        library: 'React',
     };
 
     handleChange = (e) => {
         console.log(e.target.value);
-        this.setState({
-             title: e.target.value,
-        });
+        console.log(e.target.type);
+        if(e.target.type === 'text'){
+            this.setState({
+                title: e.target.value             
+            });
+
+        }else if(e.target.type === 'textarea'){
+            this.setState({
+                text: e.target.value
+            });
+        }else if(e.target.type === 'select-one'){
+            this.setState({
+                library: e.target.value
+            });
+        }else{
+            console.log("Nothing here");
+        }
+        
     }
     
     render() {
-        const { title } = this.state;
+        const { title,text,library} = this.state;
         return(        
             <div>
                 <form>
                     <input type="text"  placeholder="Enter Value" value ={title} onChange={this.handleChange} />
                     <p>{title}</p>
+                    <br/>
+                    <br/>
+
+                    <textarea name="text" value={text} onChange={this.handleChange}/>
+
+                    <br/>
+                    <br/>
+
+                    <select value={library} onChange={this.handleChange}>
+                        <option value="React">React</option>
+                        <option value="Angular">Angular</option>
+                    </select>
+
                 </form>
             </div>
         
