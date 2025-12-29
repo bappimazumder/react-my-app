3b8c47ea8fa4d126ecc021b48c7c099dedc56bac
Bappi Mazumder <bappi.itsd@goldenharvestbd.com>
2025-12-29 14:14:34 +0600

    main: implement onclick and on mouse over

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,31 @@
 // import Calculator from './components/Calculator';
 //import Text from "./components/inheritance/Text";
 //import Emoji from "./components/inheritance/Emoji";
-import Text from "./components/composition/Text"
-import Emoji from "./components/composition/Emoji";
-import Bracket from "./components/composition/Bracket" 
+// import Text from "./components/composition/Text"
+// import Emoji from "./components/composition/Emoji";
+// import Bracket from "./components/composition/Bracket" 
+
+import ClickCounter from './components/ClickCounter'
+import HoverCounter from './components/HoverCounter'
 
 function App() {
   console.log("App component rendered");
+  return <div className="app">
+    <ClickCounter />
+    <HoverCounter />
+    </div>
  
-  return (
-    <Emoji>
-        {/* {({addEmoji}) => <Text  addEmoji={addEmoji} />}      */}
-        {({addEmoji}) => (
-          <Bracket>
-            {({ addBracket }) => <Text addEmoji={addEmoji} addBracket={addBracket} />}
-          </Bracket>
-        )}
-    </Emoji>
+  // return (
+  //   <Emoji>
+  //       {/* {({addEmoji}) => <Text  addEmoji={addEmoji} />}      */}
+  //       {({addEmoji}) => (
+  //         <Bracket>
+  //           {({ addBracket }) => <Text addEmoji={addEmoji} addBracket={addBracket} />}
+  //         </Bracket>
+  //       )}
+  //   </Emoji>
     
-  );
+  // );
  
  // return <Emoji><Text /></Emoji>; 
 
