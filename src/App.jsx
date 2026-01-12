e1edd8aceae55fe9c5ff0e1ddb9e3105f0f91bfc
Bappi Mazumder <bappi.itsd@goldenharvestbd.com>
2026-01-12 19:19:14 +0600

    main: implement useCalback and useMemo hook in react app

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,41 @@
-import React from 'react';
-import { useState } from "react";
-import MyComponent from './components/MyComponent';
+import { useState,useCallback,useMemo } from "react";
+// import MyComponent from './components/MyComponent';
+import ShowCount from './components/ShowCount';
+import Button from './components/ButtonFunc';
+import Title from './components/Title';
 
 export default function App(){
 
-  const [show,setShow] = useState(true);
+  const[count1, setcount1] = useState(0);
+  const[count2, setCount2] = useState(0);
 
-    return(
-      <div className="app">     
-        <div>{show && <MyComponent />}</div>
-        <p>
-           <button type="button" onClick={() => setShow((prevShow) => !prevShow)}>
-            {show ? 'Hide post' : 'Show post'}
-           </button>
-        </p>
-      </div>
-    );
+  const incrementByOne = useCallback(() => {
+    setcount1(prevCount => prevCount + 1);
+  },[]);
 
+  const incrementByFive = useCallback(() => {
+    setCount2(prevCount => prevCount + 5);
+  },[]);
+
+  const isEvenOrOdd = useMemo(() => {
+    let i =0;
+    while (i < 100000000) i +=1;
+    return count1 % 2 === 0;
+  },[count1]);
   
+  return(
+    <div className="app">     
+      <Title />
+
+      <ShowCount count={count1} title="Counter 1"/>
+      <span>{isEvenOrOdd ? 'Even' : 'Odd'}</span>
+      <Button handleClick={incrementByOne}>Increment by one</Button>
+      <hr/>
+
+      <ShowCount count={count2} title="Counter 2"/>
+      <Button handleClick={incrementByFive}>Increment by five</Button>
+    </div>
+  );  
 
 }
 
---
+++ src/components/ButtonFunc.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function ButtonFunc({handleClick,children}){
+
+    console.log(`rendering button ${children}`);
+
+    return(
+        <p>
+            <button type="button" onClick={handleClick}>
+                {children}
+            </button>
+        </p>
+    );
+}
+
+export default React.memo(ButtonFunc);
